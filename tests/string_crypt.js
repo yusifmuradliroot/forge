02282b44f462c3e4baaac13e311dee9ceab394a2
yusifmuradliroot <257121250+yusifmuradliroot@users.noreply.github.com>
2026-09-05 12:40:39 +0000

    empty repo, rebuild from zero

--- tests/string_crypt.js
+++
@@ -1,6 +0,0 @@
-// string_crypt torture
-const short = "abc";
-const url = "https://example.com/some/path";
-const marker = "__pixelDrawer";
-const tpl = `hello ${short} world, this is long`;
-console.log(short, url, marker, tpl);
